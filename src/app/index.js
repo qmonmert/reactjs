1bca14737b54dd96bdd3fdb52bdb3a855da8dd7d
Quentin Monmert <q.monmert@proxiad.com>
2016-09-03 13:22:03 +0200

    Passing data between parent and child components

--- src/app/index.js
+++ src/app/index.js
@@ -6,8 +6,21 @@ import { Home } from './components/home';
 
 class App extends React.Component {
 
+    constructor() {
+        super();
+        this.state = {
+            homeLink: "Home"
+        };
+    }
+
     onGreet() {
         alert("Hello");
+    }
+
+    onChangeLinkName(newName) {
+        this.setState({
+            homeLink: newName
+        });
     }
 
     render() {
@@ -15,12 +28,17 @@ class App extends React.Component {
             <div className="container">
                 <div className="row">
                     <div className="col-xs-10 col-xs-offset-1">
-                        <Header homeLink="Home"/>
+                        <Header homeLink={this.state.homeLink}/>
                     </div>
                 </div>
                 <div className="row">
                     <div className="col-xs-10 col-xs-offset-1">
-                        <Home name={"Quentin"} initialAge={27} greet={this.onGreet} />
+                        <Home 
+                            name={"Quentin"} 
+                            initialAge={27} 
+                            greet={this.onGreet}
+                            changeLink={this.onChangeLinkName.bind(this)} 
+                        />
                     </div>
                 </div>
             </div>
